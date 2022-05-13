390bc7974f96b5f5fba118ff3b13e89d16f53143
pavitpim40 <pavitpim@gmail.com>
2022-05-13 13:34:58 +0700

    finish model and association

---
+++ models/todo.js
@@ -0,0 +1,33 @@
+module.exports = (sequelize, DataTypes) => {
+  const Todo = sequelize.define(
+    "Todo",
+    {
+      title: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notEmpty: true,
+        },
+      },
+      completed: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: false,
+      },
+      dueDate: DataTypes.DATEONLY,
+    },
+    { underscored: true }
+  );
+
+  Todo.associate = function (models) {
+    Todo.belongsTo(models.User, {
+      foreignKey: {
+        allowNull: false,
+        name: "userId",
+      },
+      onDelete: "RESTRICT",
+    });
+  };
+  console.log("####", typeof Todo);
+  return Todo;
+};
